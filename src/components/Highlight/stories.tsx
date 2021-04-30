4d7df7da3d5f718ba8dfa1cb3729274175b33c5b
Yan Real <real.yan.yr@gmail.com>
2021-04-30 07:32:06 -0300

    Creating Highlight component

---
+++ src/components/Highlight/stories.tsx
@@ -0,0 +1,31 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import Highlight, { HighlightProps } from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  args: {
+    title: 'Red Dead is back!',
+    subtitle: "Come see John's new adventures",
+    buttonLabel: 'Buy now',
+    buttonLink: '/rdr2',
+    backgroundImage: '/img/red-dead-img.jpg'
+  }
+} as Meta
+
+export const Default: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+export const WithFloatImage: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+WithFloatImage.args = {
+  floatImage: '/img/red-dead-float.png'
+}
