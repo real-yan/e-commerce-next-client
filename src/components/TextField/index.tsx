22352236a8b94a0b95b3a1efbbaedcf867314368
Yan Real <real.yan.yr@gmail.com>
2021-07-02 08:19:43 -0300

    Creating Profile pages with styles and tests

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -17,7 +17,7 @@ const TextField = ({
   icon,
   iconPosition = 'left',
   label,
-  labelFor = '',
+  name,
   initialValue = '',
   error,
   disabled = false,
@@ -35,7 +35,7 @@ const TextField = ({
 
   return (
     <S.Wrapper disabled={disabled} error={!!error}>
-      {!!label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
+      {!!label && <S.Label htmlFor={name}>{label}</S.Label>}
       <S.InputWrapper>
         {!!icon && <S.Icon iconPosition={iconPosition}>{icon}</S.Icon>}
         <S.Input
@@ -44,6 +44,8 @@ const TextField = ({
           value={value}
           iconPosition={iconPosition}
           disabled={disabled}
+          name={name}
+          {...(label ? { id: name } : {})}
           {...props}
         />
       </S.InputWrapper>
